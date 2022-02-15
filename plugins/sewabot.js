7f9d756d501b476da3acf89732e10d3d631eb361
dikibotzgithub <99469121+dikibotzgithub@users.noreply.github.com>
2022-02-15 19:59:41 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,24 +3,24 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
     let kamisato = `
 ┌〔 List Berlangganan 〕
-├ 2 Bulan
-├ 1 Bulan
-├ 1 Minggu
+├ 🤴Not Berlaganan
+├ 🤴Not Berlanganan
+├ 🤴Not Berlanganan
 ├ Trial 3 Hari
 └────
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 🤴Not Berlanganan
+GoPay: 🤴Not Berlanganan
 `.trim()
     const button = {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '2 Bulan', description: "Rp15.000\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rp10.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
-        {title: '1 Minggu', description: "Rp7.000\nSewa bot selama 1 minggu.", rowId:".masuk"},
+        {title: '🤴Not', description: "RpNot\nNot Sewa Bot.", rowId:".masuk"},
+        {title: '🤴Not', description: "RpNot\nNot Sewa Bot.", rowId:".masuk"},
+        {title: '🤴Not', description: "RpNot\nNot Sewa Bot.", rowId:".masuk"},
         {title: 'Trial', description: "GRATIS\nBot gratis 3 Hari.", rowId:".join"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".snk"},
